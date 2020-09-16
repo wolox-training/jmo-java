02c1e494a8cf922c1b8a1a7ff42681bb8f3ad929
Jhoan Montoya <jhoanmontoya@Woloxs-MacBook-Pro.local>
2020-09-16 16:04:15 -0500

    Added entity book with builder pattern

--- src/main/java/com/wolox/training/TrainingApplication.java
+++ src/main/java/com/wolox/training/TrainingApplication.java
@@ -1,5 +1,6 @@
 package com.wolox.training;
 
+import com.wolox.training.model.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
