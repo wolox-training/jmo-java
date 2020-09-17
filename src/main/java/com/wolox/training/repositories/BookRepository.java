18cad09cdf69c47c4d67b633d04a3b3a4a939d62
Jhoan Montoya <jhoanmontoya@Woloxs-MacBook-Pro.local>
2020-09-17 10:24:23 -0500

    Repository query is adjusted

--- src/main/java/com/wolox/training/repositories/BookRepository.java
+++ src/main/java/com/wolox/training/repositories/BookRepository.java
@@ -4,10 +4,12 @@ import com.wolox.training.model.Book;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends CrudRepository<Book, Long> {
 
-    @Query("select b.author from Book b where b.author :author")
-    Optional<Book> findBookByAuthor(@Param("author") String author);
+    @Query("select b.author from Book b where b.author = :author")
+    Book findBookByAuthor(@Param("author") String author);
+
 }
