1b5be597e7ca9d462818494fcc73f7c816b9d549
Jhoan Montoya <jhoanmontoya@Woloxs-MacBook-Pro.local>
2020-09-18 15:01:21 -0500

    Added annotation repository

--- src/main/java/com/wolox/training/repositories/BookRepository.java
+++ src/main/java/com/wolox/training/repositories/BookRepository.java
@@ -2,13 +2,12 @@ package com.wolox.training.repositories;
 
 import com.wolox.training.model.Book;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 
-    Optional<Book> findByAuthor(@Param("author") String author);
+    Optional<Book> findByAuthor(String author);
 
 }
