224ce98c90e8ca7a3553381d60c26e744b0ccf43
Jhoan Montoya <jhoanmontoya@Woloxs-MacBook-Pro.local>
2020-09-25 09:49:20 -0500

    Constant classes are added

---
+++ src/main/java/com/wolox/training/constants/Message.java
@@ -0,0 +1,19 @@
+package com.wolox.training.constants;
+
+import org.springframework.jmx.access.InvalidInvocationException;
+
+public class Message {
+
+    private Message(){
+        throw new InvalidInvocationException(INVALID_INVOCATION);
+    }
+
+    public static final String INVALID_INVOCATION = "Invalid invocation";
+    public static final String BOOK_ALREADY_EXIST = "Book already exists.";
+    public static final String BOOK_NOT_FOUND = "Book not found";
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String USER_ALREADY_EXIST = "User already exists.";
+    public static final String NOT_FOUND = "Not found";
+    public static final String USER_HAS_NOT_BOOK = "User has not the book";
+
+}
--- src/main/java/com/wolox/training/model/Book.java
+++ src/main/java/com/wolox/training/model/Book.java
@@ -2,15 +2,11 @@ package com.wolox.training.model;
 
 import java.util.List;
 import java.util.Objects;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity
